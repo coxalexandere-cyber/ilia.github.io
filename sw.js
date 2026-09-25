e44fb5a728b63a903784494e2f6608f575a5c574
coxalexandere-cyber <coxalexandere@gmail.com>
2026-09-25 22:29:12 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='fonzel-v3',A=['./','./index.html','./html2canvas.min.js','./Vazirmatn-Regular.woff2','./Vazirmatn-Bold.woff2','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
+const V='fonzel-v6',A=['./','./index.html','./html2canvas.min.js','./Vazirmatn-Regular.woff2','./Vazirmatn-Bold.woff2','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!=V).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
